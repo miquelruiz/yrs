1da25ff7d545530fa2b9a98fe695371165db1661
Miquel Ruiz <self@miquelruiz.net>
2023-02-18 21:21:19 -0500

    Extract http handlers to their own functions

--- web/main.go
+++ web/main.go
@@ -5,13 +5,12 @@ import (
 	"fmt"
 	"html/template"
 	"log"
+	"net/http"
 	"strconv"
 	"strings"
 
 	"github.com/miquelruiz/yrs/lib"
 	"github.com/miquelruiz/yrs/yrs"
-
-	"net/http"
 )
 
 type WebYrs yrs.Yrs
@@ -56,6 +55,56 @@ func (w *WebYrs) listChannels(rw http.ResponseWriter, req *http.Request) {
 	}
 }
 
+func (w *WebYrs) listVideos(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("Serving /list-videos")
+	y := yrs.Yrs(*w)
+	videos, err := y.GetVideos()
+	if err != nil {
+		fmt.Fprintf(rw, "Error retrieving videos: %v", err)
+		return
+	}
+	last := req.URL.Query().Get("last")
+	if last != "" {
+		lastInt, err := strconv.Atoi(last)
+		if err != nil {
+			fmt.Printf("Ilegal 'last' param: %s", err)
+			lastInt = len(videos)
+		}
+		if len(videos) > lastInt {
+			videos = videos[len(videos)-lastInt:]
+		}
+	}
+	renderVideos(rw, req, videos)
+}
+
+func (w *WebYrs) update(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("Serving /update")
+	y := yrs.Yrs(*w)
+	videos, err := y.Update()
+	if err != nil {
+		fmt.Fprintf(rw, "Error updating videos: %v", err)
+		return
+	}
+	renderVideos(rw, req, videos)
+}
+
+func index(rw http.ResponseWriter, req *http.Request) {
+	fmt.Println("Serving /")
+	t, err := template.ParseFiles("templates/base.tmpl")
+	if err != nil {
+		fmt.Printf("Error parsing template: %v", err)
+		return
+	}
+
+	tplVars := map[string]interface{}{
+		"rootUrl": rootUrl,
+	}
+	if err = t.Execute(rw, tplVars); err != nil {
+		fmt.Printf("Error running template: %v", err)
+		return
+	}
+}
+
 func renderVideos(rw http.ResponseWriter, req *http.Request, videos []yrs.Video) {
 	t, err := template.ParseFiles("templates/base.tmpl", "templates/videos.tmpl")
 	if err != nil {
@@ -96,53 +145,10 @@ func main() {
 	mux.Handle(buildUrl("/js/"), http.StripPrefix(buildUrl("/js/"), http.FileServer(http.Dir("js"))))
 
 	mux.HandleFunc(buildUrl("/list-channels"), wy.listChannels)
-	mux.HandleFunc(buildUrl("/list-videos"), func(rw http.ResponseWriter, req *http.Request) {
-		fmt.Println("Serving /list-videos")
-		videos, err := y.GetVideos()
-		if err != nil {
-			fmt.Fprintf(rw, "Error retrieving videos: %v", err)
-			return
-		}
-		last := req.URL.Query().Get("last")
-		if last != "" {
-			lastInt, err := strconv.Atoi(last)
-			if err != nil {
-				fmt.Printf("Ilegal 'last' param: %s", err)
-				lastInt = len(videos)
-			}
-			if len(videos) > lastInt {
-				videos = videos[len(videos)-lastInt:]
-			}
-		}
-		renderVideos(rw, req, videos)
-	})
+	mux.HandleFunc(buildUrl("/list-videos"), wy.listVideos)
+	mux.HandleFunc(buildUrl("/update"), wy.update)
 
-	mux.HandleFunc(buildUrl("/update"), func(rw http.ResponseWriter, req *http.Request) {
-		fmt.Println("Serving /update")
-		videos, err := y.Update()
-		if err != nil {
-			fmt.Fprintf(rw, "Error updating videos: %v", err)
-			return
-		}
-		renderVideos(rw, req, videos)
-	})
-
-	mux.HandleFunc(buildUrl("/"), func(rw http.ResponseWriter, req *http.Request) {
-		fmt.Println("Serving /")
-		t, err := template.ParseFiles("templates/base.tmpl")
-		if err != nil {
-			fmt.Printf("Error parsing template: %v", err)
-			return
-		}
-
-		tplVars := map[string]interface{}{
-			"rootUrl": rootUrl,
-		}
-		if err = t.Execute(rw, tplVars); err != nil {
-			fmt.Printf("Error running template: %v", err)
-			return
-		}
-	})
+	mux.HandleFunc(buildUrl("/"), index)
 
 	addr := fmt.Sprintf("%s:%d", address, port)
 	fmt.Printf("Serving at %s/%s\n", addr, rootUrl)
